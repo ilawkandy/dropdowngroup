43ff659c14f8461e8839c572f08a90f1d062d450
ilawkandy <kapstahillar@gmail.com>
2016-12-02 10:53:22 +0200

    Update dropdowngroup.js

--- dropdowngroup.js
+++ dropdowngroup.js
@@ -1,48 +1,67 @@
-jQuery(function () {    
+/**
+ * A jQuery plugin for input-group style dropdown-group.
+ * Author: Hillar Kapsta
+ */
 
-    $.fn.dropdowngroup = function(options = []) {
+;(function($) {
+    
+    var pluginName = 'dropdowngroup';
 
-        /*Settings for this element*/
-        var defaults = {
-            /* None i could think of :'( */
-        };
+    function Plugin(element, options) {
 
-        var settings = $.extend( {}, defaults, options );
+        var el = element;
+        var $el = $(element);
 
-        $(this).find(".dropdowngroup-content").hide();
+        var title = $el.find(".dropdowngroup-select").data("title");
+        var defaultValue = "";
+        var defaultTitle = "";
+        var defaultValueSet = false;
 
-        var selectOptions = [];
+        options = $.extend({}, $.fn[pluginName].defaults, options);
 
-        $(this).find(".dropdowngroup-select option").each(function() {
+        function init() {
 
-            var selected = ($(this).hasAttr("selected")) ? true : false;
-            var optVal = {
-                value: $(this).attr("value"),
-                text: $(this).text(),
-                selected: selected
+            // Add any initialization logic here...                        
+
+            $el.find(".dropdowngroup-content").hide();
+
+            var selectOptions = [];
+
+            $el.find(".dropdowngroup-select option").each(function() {               
+
+                if (defaultValueSet == false) {   
+
+                    defaultValue = $(this).attr("value");
+
+                    defaultTitle = $(this).text();
+                    defaultValueSet = true;
+                }
+
+                var selected = ($(this).attr("selected")) ? true : false;
+                var optVal = {
+                    value: $(this).attr("value"),
+                    text: $(this).text(),
+                    selected: selected
+                };
+
+                selectOptions.push(optVal);
+            });
+
+            var data = {
+                input: {
+                    name : $el.find(".dropdowngroup-input").attr("name"),
+                    value : $el.find(".dropdowngroup-input").val(),
+                },
+                select: {
+                    name: $el.find(".dropdowngroup-select").attr("name"),
+                    title: ($el.find(".dropdowngroup-select").data("title")) ? $el.find(".dropdowngroup-select").data("title") : defaultTitle ,
+                    options: selectOptions,
+                }
             };
 
-            selectOptions.push(optVal);
-        });
-
-        var data = {
-            input: {
-                name : $(this).find(".dropdowngroup-input").attr("name"),
-                value : $(this).find(".dropdowngroup-input").val(),
-            },
-            select: {
-                name: $(this).find(".dropdowngroup-select").attr("name"),
-                title: $(this).find(".dropdowngroup-select").data("title"),
-                options: selectOptions,
-            }
-        };
-
-        //Delete previous input elements to prevent any conflicts
-        $(this).find(".dropdowngroup-input").remove();
-        $(this).find(".dropdowngroup-select").remove();
-
-
-        function getHtmlData(data) {
+            //Delete previous input elements to prevent any conflicts
+            $el.find(".dropdowngroup-input").remove();
+            $el.find(".dropdowngroup-select").remove();
 
             var optionsHtml = "";
             var select = data.select;
@@ -60,46 +79,158 @@ jQuery(function () {
                     </li>
                 `;                
             }
-
+            
             var inputHtml = `
-            <div class="input-group input-drowdown">
-                <input type="text" name="`+input.name+`" class="form-control dropdowngroup-input-value">
-                <input type="hidden" class="dropdowngroup-select-value" name="`+select.name+`" value="">
+            <div class="input-group input-dropdown">
+                <input type="text" name="`+input.name+`" class="form-control dropdowngroup-input-value">                
                 <div class="input-group-btn">
-                    <button type="button" class="btn btn-primary dropdown-toggle waves-effect waves-light input-drowdown-title" data-toggle="dropdown" aria-expanded="false">
-                    <span class="input-drowdown-text">`+input.title+`</span>
+                    <input type="hidden" class="dropdowngroup-select-value" name="`+select.name+`" value="`+defaultValue+`">
+                    <button type="button" class="btn btn-primary dropdown-toggle waves-effect waves-light input-dropdown-title" data-toggle="dropdown" aria-expanded="false">
+                    <span class="input-dropdown-text">`+select.title+`</span>
                     <span class="caret"></span>
                     </button>
-                    <ul class="dropdown-menu input-drowdown-menu" role="menu">
+                    <ul class="dropdown-menu input-dropdown-menu" role="menu">
                         `+optionsHtml+`
                     </ul>
                 </div>            
             </div>
             `;
 
-            return inputHtml;
-        }
-        
-        var htmlData = getHtmlData(data);
+            $el.append(inputHtml);              
 
-        $(this).append(htmlData);
-        return this;
+            hook('onInit');
+        }
+
+        function refresh() {
+
+            var itemVal = $el.find(".dropdowngroup-select-value").val(); 
+            var clickedItem = $el.find("li a[data-value='"+itemVal+"']").parent();
+            var clickedDropdown = clickedItem.parentsUntil(".input-dropdown");
+            
+            var clickedItemText = clickedItem.find("a").text();
+            var clickedItemVal = clickedItem.find("a").data("value");
+
+            if (clickedItemText)
+                clickedDropdown.find(".input-dropdown-text").text(clickedItemText);    
+            else    
+                clickedDropdown.find(".input-dropdown-text").text(defaultTitle);  
+
+            if (itemVal) {
+
+            } else {
+                $el.find(".dropdowngroup-select-value").val(defaultValue); 
+            }
+
+        }
+
+        function clear() {
+
+
+            $el.find(".input-dropdown-text").text(defaultTitle);
+            $el.find("input.dropdowngroup-select-value").val(defaultValue);   
+        }
+
+        function option (key, val) {
+
+            if (val) {
+                options[key] = val;
+            } else {
+                return options[key];
+            }
+        }
+
+        function destroy() {
+
+            $el.each(function() {
+                var el = this;
+                var $el = $(this);
+
+                // Add code to restore the element to its original state...
+
+                hook('onDestroy');
+                $el.removeData('plugin_' + pluginName);                
+            });
+        }
+
+        function hook(hookName) {
+            if (options[hookName] !== undefined) {
+                options[hookName].call(el);
+            }
+        }
+
+        init();
+
+        return {
+            option: option,
+            destroy: destroy,
+            refresh: refresh,
+            clear: clear,
+        };
+
+
     }
 
+    $.fn[pluginName] = function(options) {
+
+        if (typeof arguments[0] === 'string') {
+
+            var methodName = arguments[0];
+            var args = Array.prototype.slice.call(arguments, 1);
+            var returnVal;
+
+            this.each(function() {
+
+                if ($.data(this, 'plugin_' + pluginName) && 
+                    typeof $.data(this, 'plugin_' + pluginName)[methodName] === 'function') 
+                {
+                    returnVal = $.data(this, 'plugin_' + pluginName)[methodName].apply(this, args);
+                }
+
+                /* else { //Error something. Gets 2 this objects 
+
+                    throw new Error('Method ' +  methodName + ' does not exist on jQuery.' + pluginName);
+                }  */ 
+            });
+
+            if (returnVal !== undefined){
+
+                return returnVal;
+            } else {
+
+                return this;
+            }
+
+        } else if (typeof options === "object" || !options) {
+            return this.each(function() {
+                if (!$.data(this, 'plugin_' + pluginName)) {
+                    $.data(this, 'plugin_' + pluginName, new Plugin(this, options));
+                }
+            });
+        }
+    };
+
+    $.fn[pluginName].defaults = {
+        onInit: function() {},
+        onDestroy: function() {}
+    };
+
     /** Functionality for changing dropdown **/
-    $('body').on("click", ".input-drowdown-menu li",  function(e) {
+    $('body').on("click", ".input-dropdown-menu li",  function(e) {
 
         var clickedItem = $(e.currentTarget);
+        var clickedDropdown = clickedItem.parentsUntil(".input-dropdown");
+
         var clickedItemText = clickedItem.find("a").text();
         var clickedItemVal = clickedItem.find("a").data("value");
 
-        $(".input-drowdown-title .input-drowdown-text").text(clickedItemText);
-        $(".input-drowdown input.dropdowngroup-select-value").val(clickedItemVal);
+        clickedDropdown.find(".input-dropdown-text").text(clickedItemText);
+        clickedDropdown.find("input.dropdowngroup-select-value").val(clickedItemVal);
     });
 
     $.each($(".dropdowngroup"), function() {
 
-        $(this).dropdowngroup();    
+        $(this).dropdowngroup();
     });
 
-});
+
+})(jQuery);
